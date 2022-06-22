60ef7930e882d73676dedaafdd91b6a0ebe4acb9
sasha <grispy22>
2022-06-22 11:18:01 +0300

    18

--- app.ts
+++ app.ts
@@ -1,7 +1,14 @@
 
 const person = {
     name:'string',
-    age:11
+    age:11,
+    hobbies:['sports','cocies']
 }
+let favoriteActivities:string[]
+favoriteActivities = ['sports']
 
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase())
+    // console.log(hobby.map)  !! dont work
+}
 console.log(person.age)
