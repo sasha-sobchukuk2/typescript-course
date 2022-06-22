75f6dd95d9401e43de8da0b8b0c2b971b843c467
sasha <grispy22>
2022-06-22 11:57:31 +0300

    22 union type

---
+++ combine.js
@@ -0,0 +1,12 @@
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+console.log(combine(1, 2));
+console.log(combine('bro', '2'));
