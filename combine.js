7b24cb8a30489cbb90bca2f413ca8d12d760d55e
sasha <grispy22>
2022-06-22 12:08:49 +0300

    24 literal types ;in combine

--- combine.js
+++ combine.js
@@ -1,12 +1,12 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConvertion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConvertion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-console.log(combine(1, 2));
-console.log(combine('bro', '2'));
+console.log(combine('1', 2, 'as-number'));
+console.log(combine('bro', '2', 'as-string'));
--- combine.ts
+++ combine.ts
@@ -1,12 +1,16 @@
-function combine (input1 :number |string,input2:number |string){
+
+function combine (
+    input1 :number |string,
+    input2:number |string,
+    resultConvertion : 'as-number' | "as-string"){
     let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2
+    if(typeof input1 === 'number' && typeof input2 === 'number'||resultConvertion ==='as-number'){
+        result = +input1 + +input2
     }else {
         result = input1.toString() + input2.toString()
     }
     return result
 }
 
-console.log(combine(1,2))
-console.log(combine('bro','2'))
+console.log(combine(1,2,'as-number'))
+console.log(combine('bro','2','as-string'))
