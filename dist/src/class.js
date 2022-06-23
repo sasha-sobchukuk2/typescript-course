299df32425fd936ebe4775eca3fa2e57cb88d0fb
sasha <grispy22>
2022-06-23 15:21:54 +0300

    61 this

---
+++ dist/src/class.js
@@ -0,0 +1,27 @@
+"use strict";
+class Department {
+    constructor(n) {
+        this.name = "DEFAULT";
+        this.employes = [];
+        this.name = n;
+    }
+    describe() {
+        console.log(this.name);
+    }
+    addEmploys(emp) {
+        this.employes.push(emp);
+    }
+    printEmploys() {
+        console.log(this.employes.length);
+        console.log(this.employes);
+    }
+}
+const acaunting = new Department('acaun1213123213213ting');
+acaunting.addEmploys('bro');
+acaunting.addEmploys('bro2');
+acaunting.employes.push('bro3');
+acaunting.printEmploys();
+// const acaunting2 = {name:'2123', describe:acaunting.describe}
+// acaunting2.describe()
+// debugger
+//# sourceMappingURL=class.js.map
---
+++ src/class.ts
@@ -0,0 +1,33 @@
+
+
+class  Department {
+    name:string ="DEFAULT"
+    employes:string[] =[]
+
+    constructor(n:string) {
+        this.name = n
+    }
+    describe (this:Department){
+        console.log(this.name)
+    }
+    addEmploys (emp:string){
+        this.employes.push(emp)
+    }
+    printEmploys(){
+        console.log(this.employes.length)
+        console.log(this.employes)
+    }
+}
+
+const  acaunting = new Department('acaun1213123213213ting')
+acaunting.addEmploys('bro')
+acaunting.addEmploys('bro2')
+acaunting.employes.push('bro3')
+acaunting.printEmploys()
+
+// const acaunting2 = {name:'2123', describe:acaunting.describe}
+// acaunting2.describe()
+// debugger
+
+
+
