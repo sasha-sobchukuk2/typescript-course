c2ed3a8f1243fc609f956137f8da7ac4d6493102
sasha <grispy22>
2022-06-24 14:03:06 +0300

    interfaces + classes

--- dist/src/interface.js
+++ dist/src/interface.js
@@ -1,11 +1,23 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'sasha',
-    age: 26,
+/** імплемент =  обовязково включити поля інтерфейсу що імплементуємо,
+ *  можимо імплементувати одночасно з багатьох*/
+class Person {
+    constructor(n) {
+        this.age = 10;
+        this.name = n;
+    }
     greet(phrase) {
         console.log(phrase + this.name);
     }
-};
+}
+let user1;
+user1 = new Person('sasha');
+/*user1 = {
+    name: 'sasha',
+    age: 26,
+    greet(phrase: string) {
+        console.log(phrase + this.name)
+    }
+}*/
 user1.greet('hi');
 //# sourceMappingURL=interface.js.map
--- src/interface.ts
+++ src/interface.ts
@@ -1,21 +1,31 @@
+interface Greetable {
+    name: string
 
-
-interface Person {
-    name:string
-    age:number
-
-    greet(phrase:string):void
+    greet(phrase: string): void
 }
+/** імплемент =  обовязково включити поля інтерфейсу що імплементуємо,
+ *  можимо імплементувати одночасно з багатьох*/
+class Person implements Greetable {
+    name: string
+    age = 10
+    constructor(n: string) {
+        this.name = n
+    }
 
-let user1:Person
-
-user1={
-    name:'sasha',
-    age:26,
     greet(phrase: string) {
         console.log(phrase + this.name)
     }
 }
+
+let user1: Person
+user1 = new Person('sasha')
+/*user1 = {
+    name: 'sasha',
+    age: 26,
+    greet(phrase: string) {
+        console.log(phrase + this.name)
+    }
+}*/
 user1.greet('hi')
 
 
