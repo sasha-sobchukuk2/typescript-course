19cbfc3f3599fff3e499102602a2d4a60d22c60c
sasha <grispy22>
2022-06-29 20:56:38 +0300

    94 build-in generics

--- src/generics.ts
+++ src/generics.ts
@@ -1,11 +1,26 @@
+/** генерики типи що вимагають уточнень або самі є уточненнями (генерики це умовні типи) */
+//
+//
+// const names:Array<string> = []; // string[]
+//
+//  const promise:Promise<string> = new Promise((resolve, reject)=>{
+//   setTimeout(()=>resolve(' '),22)
+//  })
+//
+//
+//
+ /** як генерики працюють*/
+// function merge <T, U>(objA:T,objB:U) {
+//  return Object.assign(objA,objB)
+// }
+// const obj1 = merge({name:'sasha'},{age:10})
+// console.log(obj1.age )
 
 
 
-const names:Array<string> = []; // string[]
 
- const promise:Promise<string> = new Promise((resolve, reject)=>{
-  setTimeout(()=>resolve(' '),22)
- })
+
+
 
 
 
