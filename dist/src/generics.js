b587b6ecb4ff81e74084927a9755f2833d63986a
sasha <grispy22>
2022-06-29 21:12:44 +0300

    98 other function restriction

--- dist/src/generics.js
+++ dist/src/generics.js
@@ -16,5 +16,16 @@ function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
 const obj1 = merge({ name: 'sasha' }, { age: 2 });
-console.log(obj1.age);
+/** вписуємо шо входящий параметр може бути будь-чим(строкою ерейом і тд) що має length значення*/
+function countAndDescribe(element) {
+    let descriptionType = 'Go no value.';
+    if (element.length === 1) {
+        descriptionType = 'got 1 element';
+    }
+    else if (element.length > 1) {
+        descriptionType = 'has ' + element.length + ' elements';
+    }
+    return [element, descriptionType];
+}
+console.log(countAndDescribe('bor'));
 //# sourceMappingURL=generics.js.map
--- src/generics.ts
+++ src/generics.ts
@@ -15,11 +15,24 @@ function merge <T extends object, U extends object>(objA:T,objB:U) {
  return Object.assign(objA,objB)
 }
 const obj1 = merge({name:'sasha'}, {age:2})
-console.log(obj1.age )
+// console.log(obj1.age )
 
 
+interface Lengthy{
+ length:number
+}
+/** вписуємо шо входящий параметр може бути будь-чим(строкою ерейом і тд) що має length значення*/
+function countAndDescribe<T extends Lengthy>(element:T):[T,string  ]{
+ let descriptionType = 'Go no value.';
+ if(element.length ===1){
+  descriptionType = 'got 1 element'
+ }else if (element.length>1){
+  descriptionType = 'has '  + element.length + ' elements'
+ }
+ return [element,descriptionType]
+}
 
-
+console.log(countAndDescribe('bor'));
 
 
 
