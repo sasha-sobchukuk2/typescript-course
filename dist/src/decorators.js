d0d0bdcbfc2c1b7d18ff22f2e1795899181243cb
sasha <grispy22>
2022-06-30 16:44:44 +0300

    property decorator

--- dist/src/decorators.js
+++ dist/src/decorators.js
@@ -6,14 +6,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 function Logger(logString) {
+    console.log('це буде перше11');
     return function (constructon) {
-        console.log('LOGGER FACTORY');
+        console.log('це буде друге11');
         console.log(logString);
         console.log(constructon);
     };
 }
 function WithTemplate(template, hookId) {
+    console.log('це буде друге222');
     return function (constructor) {
+        console.log('це буде перше22');
         console.log('TEMPLATE FACTORY');
         const p = new constructor();
         const hookEl = document.getElementById(hookId);
@@ -35,4 +38,28 @@ Person1 = __decorate([
 ], Person1);
 const pers = new Person1();
 console.log(pers);
+function LogProp(target, propertyName) {
+    console.log('property decorator!');
+    console.log(target, propertyName);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    LogProp
+], Product.prototype, "title", void 0);
 //# sourceMappingURL=decorators.js.map
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,8 +1,9 @@
 
 function Logger (logString:string){
+    console.log('це буде перше11')
 
     return function (constructon:Function){
-        console.log('LOGGER FACTORY')
+        console.log('це буде друге11')
 
         console.log(logString )
         console.log(constructon)
@@ -11,7 +12,11 @@ function Logger (logString:string){
 }
 
 function WithTemplate (template:string,hookId:string){
+    console.log('це буде друге222')
+
     return function (constructor:any){
+        console.log('це буде перше22')
+
         console.log('TEMPLATE FACTORY')
         const p = new constructor()
         const hookEl = document.getElementById(hookId)
@@ -36,6 +41,35 @@ const pers = new Person1()
 console.log(pers)
 
 
+
+
+
+
+function LogProp(target: any,propertyName:string |Symbol ){
+    console.log('property decorator!');
+    console.log(target, propertyName);
+}
+
+
+class  Product {
+    @LogProp
+    title :string;
+    private _price:number
+    set price(val:number){
+        if(val>0){
+            this._price = val
+        }else {
+            throw new Error('Invalid price - should be positive!')
+        }
+    }
+    constructor(t:string,p:number) {
+        this.title = t;
+        this._price = p;
+    }
+    getPriceWithTax(tax:number){
+        return this._price * (1 + tax)
+    }
+}
 
 
 
